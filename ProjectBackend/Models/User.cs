b2bca1e5dbde152b407ac62defd6a042eadee608
ruibing <rbruibingnet@gmail.com>
2022-04-11 11:12:49 +0800

    changed passwordhash and salt to byte in usermodel

--- ProjectBackend/Models/User.cs
+++ ProjectBackend/Models/User.cs
@@ -7,8 +7,8 @@ namespace ProjectBackend.Models
         public int Id { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
-        public string PasswordHash { get; set; }
-        public string PasswordSalt { get; set; }
+        public byte PasswordHash { get; set; }
+        public byte PasswordSalt { get; set; }
 
         public int RoleId { get; set; }
         public Role Role { get; set; }
