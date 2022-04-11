91a6bab75d90460a6b3634181dad6040b1d5f4b7
ruibing <rbruibingnet@gmail.com>
2022-04-11 18:10:29 +0800

    Update UserDTO.cs

--- ProjectBackend/DTOs/UserDTO.cs
+++ ProjectBackend/DTOs/UserDTO.cs
@@ -5,6 +5,9 @@
         public string FullName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string ProfileImgUrl { get; set; }
+
         public int RoleId { get; set; }
+
     }
 }
