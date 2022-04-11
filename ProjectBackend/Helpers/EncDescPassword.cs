b422363871be5b52e7dce88e4c95ebfaedb97cd0
ruibing <rbruibingnet@gmail.com>
2022-04-11 11:14:01 +0800

    Update EncDescPassword.cs

--- ProjectBackend/Helpers/EncDescPassword.cs
+++ ProjectBackend/Helpers/EncDescPassword.cs
@@ -1,6 +1,26 @@
-﻿namespace ProjectBackend.Helpers
+﻿using System.Linq;
+using System.Security.Cryptography;
+
+namespace EmployeeManagementAPI.Helpers
 {
-    public class EncDescPassword
+    public static class EncDescPassword
     {
+
+        public static void CreateHashPassword(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+        public static bool VerifyHashPassword(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
     }
 }
